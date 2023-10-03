2846a725157ef565dad77c79087546bbb7e9e182
solahkay <solahudinalmalibari@gmail.com>
2023-10-03 09:42:10 +0700

    initialization project

---
+++ src/main/java/solahkay/binar/challenge/BinarBejChallengeGoldApplication.java
@@ -0,0 +1,13 @@
+package solahkay.binar.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarBejChallengeGoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarBejChallengeGoldApplication.class, args);
+	}
+
+}
