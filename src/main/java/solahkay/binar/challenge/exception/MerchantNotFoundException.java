e7d9c11c854ea578c9f3e0a4dc6d75b6bf4ebdcb
solahkay <solahudinalmalibari@gmail.com>
2023-10-06 22:45:42 +0700

    add MerchantNotFound and ProductNotFound exception

---
+++ src/main/java/solahkay/binar/challenge/exception/MerchantNotFoundException.java
@@ -0,0 +1,9 @@
+package solahkay.binar.challenge.exception;
+
+public class MerchantNotFoundException extends RuntimeException {
+
+    public MerchantNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/solahkay/binar/challenge/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package solahkay.binar.challenge.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
