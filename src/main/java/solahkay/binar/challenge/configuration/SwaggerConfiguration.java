1e9cc225a7b89b6014f5c44b234e2d8b671d352d
solahkay <solahudinalmalibari@gmail.com>
2023-10-23 13:42:56 +0700

    add Springfox Swagger configuration

---
+++ src/main/java/solahkay/binar/challenge/configuration/SwaggerConfiguration.java
@@ -0,0 +1,51 @@
+package solahkay.binar.challenge.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebMvc
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "BinarFud API",
+                "Binar Fud API Documentation",
+                "1.0",
+                "",
+                new Contact("Solahudin Al Malibari", "https://github.com/solahkay", ""),
+                "Creative Commons Attribution 4.0",
+                "https://creativecommons.org/licenses/by/4.0/",
+                Collections.emptyList()
+        );
+    }
+
+}
